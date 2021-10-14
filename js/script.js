f1a7ece661b6fbb639a36c44e85b791cf930ddfd
omarkhaaleed <omar309khaled@gmail.com>
2021-10-14 17:04:18 +0200

    muddaris

---
+++ js/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+
+    $('nav ul li a').click(function() {
+        $('nav ul li a').removeClass("active")
+        $(this).addClass("active")
+
+    });
+
+
+
+
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            600: {
+                items: 3,
+                nav: false
+            },
+            1000: {
+                items: 5,
+                nav: true,
+                loop: false
+            }
+        }
+    })
+
+
+
+
+
+})
